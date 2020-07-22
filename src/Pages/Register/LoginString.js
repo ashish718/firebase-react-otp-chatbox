97e9a2bcb0c5c7fbadd278b3b591c095f3d0525d
Ashish Goyal <goyal.ashish062@gmail.com>
2020-07-23 05:17:42 +0530

    after otp check user existing or not and then add user

---
+++ src/Pages/Register/LoginString.js
@@ -0,0 +1,14 @@
+const LoginString = {
+  FirstName:'firstname',
+  LastName:'lastname',
+  PhoneNo:'number',
+  PhotoURL:'photourl',
+  ID:'id',
+  FirebaseDocumentId:'docid',
+  DOC:'added',
+  UPLOAD_CHANGED:'state_changed',
+  CurrentUserdocKey:'',
+  PREFIX_IMAGE:'image/',
+}
+
+export default LoginString
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,27 +1,72 @@
 import React, {useState} from 'react';
 import firebase from '../../Services/firebase';
 import {Link} from 'react-router-dom';
-
+import LoginString from './LoginString'
 const Register = (props)=>{
 
 
-  const [phone, setPhone] = useState()
+  const [phoneno, setPhone] = useState()
   const [otp, setOtp] = useState()
 
-  
+
 
   const submit = e=>{
     e.preventDefault()
-    console.log("number", phone);
+    console.log("number", phoneno);
 
     let recaptcha = new firebase.auth.RecaptchaVerifier('recaptcha-container');
 
-    firebase.auth().signInWithPhoneNumber(phone, recaptcha)
+    firebase.auth().signInWithPhoneNumber(phoneno, recaptcha)
     .then(e=>{
     let code = prompt('enter the otp', '')
     if (code==null) return;
-    e.confirm(code).then(result=>{
+    e.confirm(code).then( async result=>{
       // console.log('user', result.user);
+      let user = result.user
+      if (user) {
+        await firebase.firestore().collection('users')
+        .where('id', '==', user.uid)
+        .get()
+        .then(userData=>{
+          console.log("userData", userData);
+          if (userData) {
+
+              userData.forEach((doc)=>{
+                console.log("doc", doc);
+
+                const currentData = doc.data();
+                console.log("currentData", currentData);
+                localStorage.setItem(LoginString.ID, currentData.id);
+                localStorage.setItem(LoginString.FirstName, currentData.firstname);
+                localStorage.setItem(LoginString.LastName, currentData.lastname);
+                localStorage.setItem(LoginString.PhotoURL, currentData.url);
+                localStorage.setItem(LoginString.PhoneNo, currentData.phoneno);
+                localStorage.setItem(LoginString.FirebaseDocumentId, doc.id);
+              })
+
+          }
+          else {
+            return firebase.firestore().collection('users')
+            .add({
+              id:result.user.uid,
+              phoneno,
+              firstname:'test',
+              lastname:'',
+              url:'',
+              messages:[{notificationId:'', number:0}]
+            })
+            .then(docRef=>{
+              console.log("docRef after adding account", docRef);
+              localStorage.setItem(LoginString.ID, result.user.uid);
+              localStorage.setItem(LoginString.FirstName, '');
+              localStorage.setItem(LoginString.LastName, '');
+              localStorage.setItem(LoginString.PhotoURL, '');
+              localStorage.setItem(LoginString.PhoneNo, phoneno);
+              localStorage.setItem(LoginString.FirebaseDocumentId, docRef.id);
+            })
+          }
+        })
+      }
       props.history.push('/chat')
     })
     .catch(error=>{
